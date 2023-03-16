9e0b7cb42c49803dead71a07d3783ddcb8cf796c
vf88 <valentin@aseno.de>
2023-03-16 15:55:36 +0100

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Wow
+    */
+    new WOW().init();
+    
+    
+	
+	/*
+	    Subscription form
+	*/
+	$('.success-message').hide();
+	$('.error-message').hide();
+	
+	
+    
+});
+
