aafbc5509a558e0cd006a7d89991320f055f203e
Joysankar Sengupta <joysankar.sengupta@technicise.com>
2023-11-28 22:09:12 +0530

    added services area

--- components/HealthcareServices/index.jsx
+++ components/HealthcareServices/index.jsx
@@ -11,7 +11,7 @@ const HealthcareServices = () => {
       >
         <div className="container">
           <SectionTitle
-            title="Health care technology and business services"
+            title="Healthcare technology and business services"
             paragraph="We offer personalized solutions and consulting services, aiming to deliver instant and concrete advantages to both our clients and the individuals they serve. Our commitment lies in leveraging our expertise in data and customer experience to enhance the accessibility of high-quality healthcare."
             center
           />
