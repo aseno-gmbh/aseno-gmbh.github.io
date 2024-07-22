7f44ded0d248751905ab222b46043b48b33ee60d
Mira Edelstein <mira.edelstein@aseno.de>
2024-07-22 15:40:18 +0200

    rename about to services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,18 +1,18 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import ServicesSectionOne from "@/components/Services/ServicesSectionOne";
+import ServicesSectionTwo from "@/components/Services/ServicesSectionTwo";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 
-const AboutPage = () => {
+const ServicesPage = () => {
   return (
     <>
       <Breadcrumb
         pageName="Services"
         description="ASENO helps all businesses not only reach, but exceed their goals. Our experience helps us lay out a strategy that perfectly fits our clients. This collaboration is essential for the successful transition from strategy, to plan, to action. Use our free webinar to see what we can do for you."
       />
-      <AboutSectionOne />
-      <AboutSectionTwo />
+      <ServicesSectionOne />
+      <ServicesSectionTwo />
     </>
   );
 };
 
-export default AboutPage;
+export default ServicesPage;
