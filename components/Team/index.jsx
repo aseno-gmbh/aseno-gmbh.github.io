f993f1cb88a1865a1804d1592ce594c09e7afef9
Mira Edelstein <mira.edelstein@aseno.de>
2024-07-24 09:27:26 +0200

    add parameters to feature component, so it can be used easely for different data sets

--- components/Team/index.jsx
+++ components/Team/index.jsx
@@ -1,29 +1,13 @@
-import SectionTitle from "../Common/SectionTitle";
-import SingleMember from "./SingleMember";
+import Features from "../Features";
 import teamData from "./TeamData";
 
 const Team = () => {
   return (
-    <>
-      <section
-        id="Team"
-        className="bg-primary/[.03] py-16 md:py-20 lg:py-28"
-      >
-        <div className="container">
-          <SectionTitle
-            title="Team"
-            paragraph="Meet our team! Expertise is not merely a possession but a tool wielded with finesse, shaping the digital healthcare future with every innovation they conceive."
-            center
-          />
-
-          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
-            {teamData.map((member) => (
-              <SingleMember key={member.id} member={member} />
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
+      <Features 
+          title={"Team"}
+          paragraph={"Meet our team! Expertise is not merely a possession but a tool wielded with finesse, shaping the digital healthcare future with every innovation they conceive."}
+          featuresData={teamData}
+      />
   );
 };
 
